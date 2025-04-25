9b76ebab602a665aa667f3b8be50f539fe220ad5
dellala21 <dellala678@gmail.com>
2025-04-25 18:34:47 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Chatbot Rasa sedang berjalan di Heroku!"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    return "Webhook diterima!"
+
+if __name__ == '__main__':
+    # Jalankan Rasa server di background
+    subprocess.Popen(["rasa", "run", "--enable-api", "--cors", "*", "--connector", "telegram"])
+    app.run(host='0.0.0.0', port=5000)
